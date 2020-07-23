8e4360cb2dff1d81e4c5eda8f777082957bb41f7
[ryanEOS] <[rjpili@live.com]>
2020-07-22 23:48:55 -0700

    remapping works, needed to inplace the .replace

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set_style('darkgrid')
 
-df = pd.read_csv("data.csv")
-df.head()
+df = pd.read_csv("data.csv", header=None)
+df.rename(columns={0:"character"}, inplace=True)
+df = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+print(df.head()) 
+
 
 tier_dict = {
+    "S+":14,
     "S":13,
     "S-":12,
     "A+":11,
@@ -21,6 +25,14 @@ tier_dict = {
     "C-":3,
     "D+":2,
     "D":1
-}
+    }
 
-df.map(tier_dict)
+#for column in df.columns:
+#    df[column].map(tier_dict)
+
+#df[1].map(tier_dict)
+#print(df.iloc[5,1], type(df.iloc[5,1]))
+
+df = df.replace(tier_dict)
+
+print(df)
